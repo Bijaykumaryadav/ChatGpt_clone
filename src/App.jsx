06f4bd73df87e8c22e8a788ff2a3aaebc5a2c6e2
bijay kumar yadav <ybijayyadav468@gmail.com>
2024-07-24 09:31:42 +0530

    repository setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-black">
+        <h1 className="bg-white">Hey this is mr. biay kumar yadav</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
